7362c0e8c9121fe3e42a2ec84800cfb49993bd38
Yana Redkina <yana.redkina@icloud.com>
2020-09-18 16:35:07 +0300

    commit 3.21(3)

--- Exercises.c
+++ Exercises.c
@@ -1,6 +1,7 @@
 /* VVO-2course- Redkina Yana - Exercises from textbook RUDENKO*/
 
 #include <stdio.h>
+#include <math.h>
 
 /* N 2.29 - Циклически сдвинуть значение целочисленной величины на n позиций влево */
 unsigned int mov_nbit(unsigned int x, unsigned char n) {
@@ -27,11 +28,22 @@ unsigned int get_digit(unsigned int n) {
         return 1;
     }
     unsigned int count = 0;
-    while (n) {
+    while (n > 0) {
         n /= 10;
         count++;
     }
    return count;
+}
+
+/* 3.31. Дано натуральное число n. Получить наименьшее число вида 2^r, превосходящее n */
+
+unsigned int power_of_two(unsigned int n) {
+    unsigned int power_count = 0;
+    while (n > 1) {
+        n /=  2;
+        power_count++;
+    }
+    return 2 << (power_count); /* pow(2,power_count+1) */
 }
 
 int main () {
@@ -40,5 +52,9 @@ int main () {
     printf("new value = %u\n", mov_nbit(3758096384,3));
     
     /* 3.21 */
-    printf("number of digits = %u\n", get_digit(2456));
+    printf("number of digits = %u\n", get_digit(22));
+    
+    /* 3.31 */
+    printf("2r = %u\n", power_of_two(23));
+
 }
