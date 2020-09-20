095f32a331c08c038b8795e1be15a3af71433e8d
Yana Redkina <yana.redkina@icloud.com>
2020-09-20 23:09:50 +0300

    task_2.1 fixes

--- task_2.1.c
+++ task_2.1.c
@@ -2,14 +2,12 @@
 
 #include <stdio.h>
 
-double eps, x;
-
-double sqrt_newton (double x, double eps) {
+double sqrt_newton(double x, double eps) {
     double cur_x = 1.0, nxt_x;
     
     nxt_x = (cur_x + x / cur_x) / 2.0;
     
-    while ((cur_x - nxt_x) >= eps || (nxt_x - cur_x) >= eps ) {
+    while ((cur_x - nxt_x) >= eps || (nxt_x - cur_x) >= eps) {
         cur_x = nxt_x;
         nxt_x = (cur_x + x / cur_x) / 2.0;
     }
@@ -20,11 +18,13 @@ double sqrt_newton (double x, double eps) {
     
 int main () {
     
+    double eps, x;
+    
     printf("input epsilon: \n");
     scanf("%lf", &eps);
     
     printf("input x: \n");
-    while ( scanf("%lf", &x) == 1) {
+    while (scanf("%lf", &x) == 1) {
          printf("sqrt from x is = %.10g\n", sqrt_newton(x, eps));
     }
 }
---
+++ task_2.2.c
@@ -0,0 +1,3 @@
+/* VVO-2course- Redkina Yana - TASK 2.2 */
+
+#include <stdio.h>
