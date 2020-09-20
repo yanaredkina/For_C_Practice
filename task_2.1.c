90efefc707338ff68c97323e6dba724fea8640b2
Yana Redkina <yana.redkina@icloud.com>
2020-09-20 23:04:23 +0300

    adding task_2.1

---
+++ task_2.1.c
@@ -0,0 +1,30 @@
+/* VVO-2course- Redkina Yana - TASK 2.1 */
+
+#include <stdio.h>
+
+double eps, x;
+
+double sqrt_newton (double x, double eps) {
+    double cur_x = 1.0, nxt_x;
+    
+    nxt_x = (cur_x + x / cur_x) / 2.0;
+    
+    while ((cur_x - nxt_x) >= eps || (nxt_x - cur_x) >= eps ) {
+        cur_x = nxt_x;
+        nxt_x = (cur_x + x / cur_x) / 2.0;
+    }
+    
+    return nxt_x;
+}
+    
+    
+int main () {
+    
+    printf("input epsilon: \n");
+    scanf("%lf", &eps);
+    
+    printf("input x: \n");
+    while ( scanf("%lf", &x) == 1) {
+         printf("sqrt from x is = %.10g\n", sqrt_newton(x, eps));
+    }
+}
