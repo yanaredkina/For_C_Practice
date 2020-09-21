afd85d2b1d7410eb3b63230f488ecb5478c8543d
Yana Redkina <yana.redkina@icloud.com>
2020-09-21 11:02:38 +0300

    adding task_2.2

--- task_2.2.c
+++ task_2.2.c
@@ -1,3 +1,20 @@
 /* VVO-2course- Redkina Yana - TASK 2.2 */
 
 #include <stdio.h>
+
+int main () {
+    
+    double x, a;
+
+    printf("input x: \n");
+    scanf("%lf", &x);
+    
+    printf("input a(i): \n");
+    
+    double res = 0;
+    while (scanf("%lf", &a) == 1) {
+        res = a + res * x;
+    }
+    
+    printf(" Horner's method Polynomial value is =  %.10g\n", res);
+}
